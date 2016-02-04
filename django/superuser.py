5a016e7335bd5be9879e940287c8d5b36c35d544
vchrisb <vchrisb@users.noreply.github.com>
2016-02-04 16:08:05 +0100

    fix

--- django/superuser.py
+++ django/superuser.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
 
 if not User.objects.filter(username='admin').exists():
-	print("------ creating admin------")
-    User.objects.create_superuser('admin', 'admin@domain.local', 'admin')
+	User.objects.create_superuser('admin', 'admin@domain.local', 'admin')
