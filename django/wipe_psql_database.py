998da6c3ef60d19f53a8d0e594de34ca9d5dd134
vchrisb <vchrisb@users.noreply.github.com>
2016-02-05 12:22:59 +0100

    table_type

--- django/wipe_psql_database.py
+++ django/wipe_psql_database.py
@@ -39,7 +39,7 @@ try:
         cur.execute("GRANT ALL ON SCHEMA public TO public")
         cur.execute("COMMENT ON SCHEMA public IS 'standard public schema'")
     else:
-        cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name")
+        cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE' ORDER BY table_name")
         rows = cur.fetchall()
         for row in rows:
             print ("dropping table: %s" %(row[0]))
