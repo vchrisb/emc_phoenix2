3073382d6601b7512c914337b80f88303a7cb3c9
vchrisb <vchrisb@users.noreply.github.com>
2016-03-21 22:04:18 +0100

    initial

--- content/tests.py
+++ content/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test.client import Client
+from django.contrib.auth.models import User
+from allauth.account.models import EmailAddress
+
 
 # Create your tests here.
+class SimpleViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('john', 'lennon@emc.com', 'johnpassword')
+        # EmailAddress.objects.create(user=self.user,
+        #                                 email='lennon@emc.com',
+        #                                 primary=True,
+        #                                 verified=True)
+
+    def test_home_view_(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_faq_view_(self):
+        response = self.client.get(reverse('faq'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_agenda_view_(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get(reverse('agenda'))
+        self.assertEqual(response.status_code, 200)
+        #print(response.content)
